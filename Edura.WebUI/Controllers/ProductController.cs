8443ef7d8550031f2f86257b2551583aba98b3da
Baki OZTURK <22.exe.94@gmail.com>
2021-05-20 21:59:07 +0300

    ProductDetail Settings

---
+++ Edura.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Edura.Repository.Abstract;
+using Edura.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edura.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductController(IProductRepository _productRepository)
+        {
+            productRepository = _productRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = productRepository
+                .GetAll()
+                .Where(x => x.Id == id)
+                .Include(x => x.Categories)
+                //.ThenInclude(x=>x.)
+                .Include(x => x.Images)
+                .Include(x => x.ProductDetails)
+                .Select(x =>
+                new ProductDetailsModel()
+                {
+                    Product = x,
+                    Images = x.Images,
+                    ProductDetails = x.ProductDetails,
+                    Categories = x.Categories
+                }).FirstOrDefault();
+            return View(model);
+        }
+    }
+}
---
+++ Edura.WebUI/Models/ProductDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using Edura.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edura.WebUI.Models
+{
+    public class ProductDetailsModel
+    {
+        public Product  Product { get; set; }
+        public List<Image> Images { get; set; }
+        public List<ProductDetails> ProductDetails { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
